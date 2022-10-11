49136b960ce00d8aad2f16601703de2f363eb80e
Yoonjiwon <k112751@koreainvestment.com>
2022-10-11 11:27:22 +0900

    Initial Commit

---
+++ ShareDataWithWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareDataWithWatch
+//
+//  Created by iMac on 2022/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
